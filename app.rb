7c17a6d26b3a1ab302dd0246a97bcf071fca6435
Tung Nguyen <tongueroo@gmail.com>
2026-08-24 03:08:01 +0000

    Add zero-downtime QA release A fixture

--- app.rb
+++ app.rb
@@ -2,6 +2,12 @@ require "sinatra"
 require "json"
 require "time"
 require "net/http"
+require "socket"
+
+QA_RELEASE = "A"
+QA_READY_AFTER_SECONDS = 3
+QA_NEVER_READY = false
+QA_BOOTED_AT = Time.now.utc
 
 # Configure Sinatra
 set :port, ENV["PORT"] || 3000
@@ -11,6 +17,32 @@ set :public_folder, "public"
 # https://github.com/sinatra/sinatra/issues/2065
 # disable it for all environments
 set :host_authorization, { permitted_hosts: [] }
+
+helpers do
+  def qa_ready?
+    !QA_NEVER_READY && (Time.now.utc - QA_BOOTED_AT) >= QA_READY_AFTER_SECONDS
+  end
+
+  def qa_response
+    {
+      release: QA_RELEASE,
+      ready: qa_ready?,
+      served_at: Time.now.utc.iso8601(6),
+      booted_at: QA_BOOTED_AT.iso8601(6),
+      replica: Socket.gethostname
+    }
+  end
+end
+
+before do
+  headers "X-QA-Release" => QA_RELEASE
+end
+
+get "/ready" do
+  content_type :json
+  status qa_ready? ? 200 : 503
+  qa_response.to_json
+end
 
 # API Routes
 get "/api/hello" do
@@ -51,5 +83,6 @@ end
 
 # Serve static files
 get "/" do
-  send_file File.join(settings.public_folder, "index.html")
+  content_type :json
+  qa_response.to_json
 end
